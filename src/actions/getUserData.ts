4df2d0624cc4406d4003e55e9562eefe4825d1b3
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-07-02 16:33:56 -0700

    janky redirect

--- src/actions/getUserData.ts
+++ src/actions/getUserData.ts
@@ -1,12 +1,31 @@
 import { currentUser } from "@clerk/nextjs";
 import {redirect} from 'next/navigation'
-
+import prisma from "@/lib/prisma";
 export async function getUserData() {
     const user = await currentUser();
 
     if(!user) {
         redirect('/sign-in')
     }
+
+    if (user) {
+        const userData = await prisma.user.findUnique({
+            where: {
+                clerkId: user.id,
+            },
+            
+        });
+
+        if (!userData) {
+            redirect('/user-profile')
+        }
+
+        if(userData) {
+            redirect('/');
+        }
+    }
+
+    
 
     return user;
 };

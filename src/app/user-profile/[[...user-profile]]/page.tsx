e12db146fd3a3018af290c2b5196590e3fbb6d88
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-07-02 10:46:44 -0700

    user profile page

--- src/app/user-profile/[[...user-profile]]/page.tsx
+++ src/app/user-profile/[[...user-profile]]/page.tsx
@@ -2,7 +2,7 @@ import { UserProfile } from "@clerk/nextjs";
 
 
 export default function UserProfilePage() {
-
+    
     return (
         <UserProfile path="/user-profile" routing="path" />
     )
